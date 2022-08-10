4ba717360427354e86d75c77e2cfb70938dcd540
Victor H <victor@martin.arch>
2022-08-10 16:15:36 -0300

    conclusão

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'username não localizado.' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user) {
+    return response.status(404).json({ error: 'user não localizado.' })
+  }
+
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'user gratuíto já possui 10 todos.' })
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User não localizado' })
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'ID inválido' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo não localizado' })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+ const { id } = request.params
+
+ const user = users.find(user => user.id === id)
+
+ if (!user) {
+  return response.status(404).json({ error: 'id não localizado.' })
+ }
+
+ request.user = user
+
+ return next()
 }
 
 app.post('/users', (request, response) => {
@@ -31,7 +82,7 @@ app.post('/users', (request, response) => {
   const usernameAlreadyExists = users.some((user) => user.username === username);
 
   if (usernameAlreadyExists) {
-    return response.status(400).json({ error: 'Username already exists' });
+    return response.status(400).json({ error: 'Username already exists.' });
   }
 
   const user = {
